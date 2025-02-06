163cbb4953ff98325fe1c95b345a468b25451e73
Chris <chl9430@gmail.com>
2025-02-07 02:32:52 +0900

    ClientSession의 모든 작업들을 한개의 스레드만 처리할 수 있는 JobQueue생성.

--- Server/GameRoom.cs
+++ Server/GameRoom.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ServerCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,15 @@ using System.Threading.Tasks;
 
 namespace Server
 {
-    class GameRoom
+    class GameRoom : IJobQueue
     {
         List<ClientSession> _sessions = new List<ClientSession>();
-        object _lock = new object();
+        JobQueue _jobQueue = new JobQueue();
+
+        public void Push(Action job)
+        {
+            _jobQueue.Push(job);
+        }
 
         // 특정 클라이언트로부터 패킷을 받으면 그 해당 패킷을
         // 방에 존재하는 모든 클라이언트 세션에게 채팅 내용을 뿌려주는 함수
@@ -20,28 +26,19 @@ namespace Server
             packet.chat = $"{chat} I am {packet.playerId}";
             ArraySegment<byte> segment = packet.Write();
 
-            lock (_lock)
-            {
-                foreach (ClientSession s in _sessions)
-                    s.Send(segment);
-            }
+            foreach (ClientSession s in _sessions)
+                s.Send(segment);
         }
 
         public void Enter(ClientSession session)
         {
-            lock (_lock)
-            {
-                _sessions.Add(session);
-                session.Room = this;
-            }
+            _sessions.Add(session);
+            session.Room = this;
         }
 
         public void Leave(ClientSession session)
         {
-            lock (_lock)
-            {
-                _sessions.Remove(session);
-            }
+            _sessions.Remove(session);
         }
     }
 }
