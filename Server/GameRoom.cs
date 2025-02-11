0b8d629c3730a0ea0796cd326232c081656457f0
JUNG HOO CHOI <chl9430@gmail.com>
2025-02-12 05:03:01 +0900

    유니티 미니프로젝트를 위한 서버의 기초 작업. Packet 내용 수정 및 그에따른 PacketFormat과 PacketHandler 수정. 패킷을 처리하는 코드 추가.

--- Server/GameRoom.cs
+++ Server/GameRoom.cs
@@ -23,31 +23,72 @@ namespace Server
             foreach (ClientSession s in _sessions)
                 s.Send(_pendingList);
 
-            Console.WriteLine($"Flushed {_pendingList.Count} items");
+            // Console.WriteLine($"Flushed {_pendingList.Count} items");
             _pendingList.Clear();
         }
 
         // 특정 클라이언트로부터 패킷을 받으면 그 해당 패킷을
         // 방에 존재하는 모든 클라이언트 세션에게 채팅 내용을 뿌려주는 함수
-        public void Broadcast(ClientSession session, string chat)
+        public void Broadcast(ArraySegment<byte> segment)
         {
-            S_Chat packet = new S_Chat();
-            packet.playerId = session.SessionId;
-            packet.chat = $"{chat} I am {packet.playerId}";
-            ArraySegment<byte> segment = packet.Write();
-
             _pendingList.Add(segment);
         }
 
         public void Enter(ClientSession session)
         {
+            // 플레이어 추가하고
             _sessions.Add(session);
             session.Room = this;
+
+            // 신입생한테 모든 플레이어 목록 전송
+            S_PlayerList players = new S_PlayerList();
+            foreach (ClientSession s in _sessions)
+            {
+                players.players.Add(new S_PlayerList.Player()
+                {
+                    isSelf = (s == session),
+                    playerId = s.SessionId,
+                    posX = s.PosX,
+                    posY = s.PosY,
+                    posZ = s.PosZ,
+                });
+            }
+            session.Send(players.Write());
+
+            // 신입생 입장을 모두에게 알린다(브로드캐스트)
+            S_BroadcastEnterGame enter = new S_BroadcastEnterGame();
+            enter.playerId = session.SessionId;
+            enter.posX = 0;
+            enter.posY = 0;
+            enter.posZ = 0;
+            Broadcast(enter.Write());
         }
 
         public void Leave(ClientSession session)
         {
+            // 플레이어 제거하고
             _sessions.Remove(session);
+
+            // 모두에게 알린다
+            S_BroadcastLeaveGame leave = new S_BroadcastLeaveGame();
+            leave.playerId = session.SessionId;
+            Broadcast(leave.Write());
+        }
+
+        public void Move(ClientSession session, C_Move packet)
+        {
+            // 좌표 바꿔주고
+            session.PosX = packet.posX;
+            session.PosY = packet.posY;
+            session.PosZ = packet.posZ;
+
+            // 모두에게 알린다
+            S_BroadcastMove move = new S_BroadcastMove();
+            move.playerId = session.SessionId;
+            move.posX = packet.posX;
+            move.posY = packet.posY;
+            move.posZ = packet.posZ;
+            Broadcast(move.Write());
         }
     }
 }
